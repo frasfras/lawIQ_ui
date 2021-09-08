0181bce1929d68bac4bedef2f3b0eb096f08849f
frasfras <65541080+frasfras@users.noreply.github.com>
2021-09-08 17:28:07 +0800

    Update rollup.config.js

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,8 +1,8 @@
 // rollup.config.js
 import vue from 'rollup-plugin-vue';
-import buble from 'rollup-plugin-buble';
+import buble from '@rollup/plugin-buble';
 import commonjs from 'rollup-plugin-commonjs';
-import replace from 'rollup-plugin-replace';
+import replace from '@rollup/plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 import minimist from 'minimist';
 
